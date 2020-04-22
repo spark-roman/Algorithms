4af8b5bc147b59140f157e875f963ad17f4f5758
Roman Akhunov <roman.akhunov@gmail.com>
2020-04-22 19:06:19 +0300

    algo-sort- buuble sorting added

---
+++ AlgoTests/Sortings/BubbleSortingTests.cs
@@ -0,0 +1,37 @@
+using System;
+using Algorithms.Sortings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTests.Sortings
+{
+    [TestClass]
+    public class BubbleSortingTests : SertingTests
+    {
+        [TestMethod]
+        public void Sort_PassEmptyArray_ReturnEmptyArray()
+        {
+            var bubbleSorting = new BubbleSort();
+            var sortedArray = bubbleSorting.Sort(_emptyArray, 0, _emptyArray.Length - 1);
+
+            CollectionAssert.AreEqual(sortedArray, _emptyArray);
+        }
+
+        [TestMethod]
+        public void Sort_PassOneElementArray_ReturnOneElementArray()
+        {
+            var bubbleSorting = new BubbleSort();
+            var sortedArray = bubbleSorting.Sort(_oneElementArray, 0, _oneElementArray.Length - 1);
+
+            CollectionAssert.AreEqual(sortedArray, _oneElementArray);
+        }
+
+        [TestMethod]
+        public void Sort_PassValidArray_ReturnSortedArray()
+        {
+            var bubbleSorting = new BubbleSort();
+            var sortedArray = bubbleSorting.Sort(_toSortArray, 0, _toSortArray.Length - 1);
+
+            CollectionAssert.AreEqual(sortedArray, _referenceSortedArray);
+        }
+    }
+}
---
+++ Algorithms/Sortings/Comparison/BubbleSort.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics.CodeAnalysis;
+using Algorithms.Utils;
+
+namespace Algorithms.Sortings
+{
+    public class BubbleSort : ICompareSortable
+    {
+        public int[] Sort([NotNull]int[] array, int first, int last)
+        {
+            var length = last - first + 1;
+
+            for (var n = length - 1; 0 < n; n--)
+            {
+                for (var j = 0; j < length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        RelatedActionsHelper.Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
